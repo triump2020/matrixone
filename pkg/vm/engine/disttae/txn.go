6246defbc2a3913945aa44f1a290ad13fa0890d6
triump2020 <triump2020@outlook.com>
2023-05-12 11:27:01 +0800

    fix datarace

--- pkg/vm/engine/disttae/txn.go
+++ pkg/vm/engine/disttae/txn.go
@@ -69,7 +69,7 @@ func (txn *Transaction) getBlockInfos(
 
 // detecting whether a transaction is a read-only transaction
 func (txn *Transaction) ReadOnly() bool {
-	return txn.readOnly
+	return txn.readOnly.Load()
 }
 
 // WriteBatch used to write data to the transaction buffer
@@ -89,7 +89,7 @@ func (txn *Transaction) WriteBatch(
 	primaryIdx int, // pass -1 to indicate no primary key or disable primary key checking
 	insertBatchHasRowId bool,
 	truncate bool) error {
-	txn.readOnly = false
+	txn.readOnly.Store(false)
 	bat.Cnt = 1
 	txn.Lock()
 	if typ == INSERT {
@@ -267,7 +267,7 @@ func (txn *Transaction) WriteFile(typ int, databaseId, tableId uint64,
 	} else if typ == INSERT {
 		txn.updatePosForCNBlock(bat.GetVector(0), idx)
 	}
-	txn.readOnly = false
+	txn.readOnly.Store(false)
 	txn.writes = append(txn.writes, Entry{
 		typ:          typ,
 		tableId:      tableId,
