a04415d78ea682e471c7e30d6f40d8997bc033b5
triump2020 <triump2020@outlook.com>
2023-05-15 16:26:01 +0800

    add log for issue 9399

--- pkg/testutil/util_debug.go
+++ pkg/testutil/util_debug.go
@@ -16,6 +16,9 @@ package testutil
 
 import (
 	"fmt"
+	"github.com/matrixorigin/matrixone/pkg/container/types"
+	"github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/logutil"
 
 	"github.com/matrixorigin/matrixone/pkg/container/batch"
 )
@@ -29,12 +32,49 @@ func OperatorCatchBatch(operatorName string, bat *batch.Batch) string {
 	if bat == nil {
 		return ""
 	}
+	var rowIdIdx int = -1
 	str := fmt.Sprintf("`%s` operator catch a batch, batch length is %d\n", operatorName, bat.Length())
 	for i, vec := range bat.Vecs {
 		str += fmt.Sprintf("[vec-%d(%s)[type is %v, %p] : len is %d]\n", i,
 			bat.Attrs[i],
 			vec.GetType().Oid, vec,
 			vec.Length())
+		if vec.GetType().Oid == types.T_Rowid {
+			rowIdIdx = i
+		}
+	}
+	if rowIdIdx != -1 {
+		// output first 10 rows
+		vec := bat.GetVector(int32(rowIdIdx))
+		rowIds := vector.MustFixedCol[types.Rowid](vec)
+		rows := 10
+		for i, rowId := range rowIds {
+			if i >= rows {
+				break
+			}
+			str += fmt.Sprintf("[rowId is %s]", rowId.String())
+		}
 	}
 	return str
+}
+
+func DebugBlockRowId(columnBatch *batch.Batch) {
+	var str string
+	for _, vec := range columnBatch.Vecs {
+		if vec.GetType().Oid == types.T_Rowid {
+			// output first 2 rows
+			rowIds := vector.MustFixedCol[types.Rowid](vec)
+			rows := 10
+			for i, rowId := range rowIds {
+				if i >= rows {
+					break
+				}
+				str += fmt.Sprintf("[rowId is %s]\n", rowId.String())
+			}
+			break
+		}
+	}
+	if str != "" {
+		logutil.Infof("block read rowId is %s", str)
+	}
 }
