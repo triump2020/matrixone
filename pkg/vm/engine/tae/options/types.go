35b4caeea042345745961edc89fbbe4751ff8745
triump2020 <triump2020@outlook.com>
2024-06-05 16:55:03 +0800

    add log and change config

--- pkg/vm/engine/tae/options/types.go
+++ pkg/vm/engine/tae/options/types.go
@@ -41,7 +41,7 @@ const (
 	DefaultCheckpointMinCount           = int64(100)
 	DefaultCheckpointIncremetalInterval = time.Minute
 	DefaultCheckpointGlobalMinCount     = 10
-	DefaultGlobalVersionInterval        = time.Hour
+	DefaultGlobalVersionInterval        = 5*time.Minute
 	DefaultGCCheckpointInterval         = time.Minute
 	DefaultOverallFlushMemControl       = common.Const1GBytes
 
