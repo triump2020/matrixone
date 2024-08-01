b01453a47b448a19046d0ffe6eaac18e4894e93a
XuPeng-SH <xupeng3112@163.com>
2024-08-01 16:16:25 +0800

    update datasource marshal

--- pkg/vm/engine/disttae/datasource.go
+++ pkg/vm/engine/disttae/datasource.go
@@ -101,42 +101,40 @@ func (tomb *tombstoneDataWithDeltaLoc) UnmarshalBinary(buf []byte) error {
 	cnt := types.DecodeUint32(buf)
 	buf = buf[4:]
 
-	tomb.inMemTombstones = make(map[types.Blockid][]int32)
+	tomb.inMemTombstones = make(map[types.Blockid][]int32, int(cnt))
 	for i := 0; i < int(cnt); i++ {
 		bid := types.DecodeFixed[types.Blockid](buf[:types.BlockidSize])
 		buf = buf[types.BlockidSize:]
 
-		len := types.DecodeUint32(buf)
+		offsetLen := types.DecodeUint32(buf)
 		buf = buf[4:]
-
-		for j := 0; j < int(len); j++ {
-			o := types.DecodeInt32(buf[:4])
-			buf = buf[4:]
-			tomb.inMemTombstones[bid] = append(tomb.inMemTombstones[bid], o)
+		if offsetLen > 0 {
+			tomb.inMemTombstones[bid] = types.DecodeSlice[int32](buf[:offsetLen])
+			buf = buf[offsetLen:]
 		}
 	}
 
 	cnt = types.DecodeUint32(buf)
 	buf = buf[4:]
-	tomb.blk2UncommitLoc = make(map[types.Blockid][]objectio.Location)
-
+	tomb.blk2UncommitLoc = make(map[types.Blockid][]objectio.Location, int(cnt))
 	for i := 0; i < int(cnt); i++ {
 		bid := types.DecodeFixed[types.Blockid](buf[:types.BlockidSize])
 		buf = buf[types.BlockidSize:]
-
-		len := types.DecodeUint32(buf)
+		locLen := types.DecodeUint32(buf)
 		buf = buf[4:]
-
-		for j := 0; j < int(len); j++ {
-			loc := buf[:objectio.LocationLen]
-			buf = buf[objectio.LocationLen:]
-			tomb.blk2UncommitLoc[bid] = append(tomb.blk2UncommitLoc[bid], loc)
+		if locLen > 0 {
+			locs := make([]objectio.Location, int(locLen)/objectio.LocationLen)
+			for j := range locs {
+				locs[j] = buf[:objectio.LocationLen]
+				buf = buf[objectio.LocationLen:]
+			}
+			tomb.blk2UncommitLoc[bid] = locs
 		}
 	}
 
 	cnt = types.DecodeUint32(buf)
 	buf = buf[4:]
-	tomb.blk2CommitLoc = make(map[types.Blockid]logtailreplay.BlockDeltaInfo)
+	tomb.blk2CommitLoc = make(map[types.Blockid]logtailreplay.BlockDeltaInfo, int(cnt))
 
 	for i := 0; i < int(cnt); i++ {
 		bid := types.DecodeFixed[types.Blockid](buf[:types.BlockidSize])
@@ -152,101 +150,83 @@ func (tomb *tombstoneDataWithDeltaLoc) UnmarshalBinary(buf []byte) error {
 			Cts: cts,
 			Loc: loc,
 		}
-
 	}
 
 	return nil
-
 }
 
-func (tomb *tombstoneDataWithDeltaLoc) MarshalBinaryWithBuffer(w *bytes.Buffer) (uint32, error) {
-
-	var size uint32
+func (tomb *tombstoneDataWithDeltaLoc) MarshalBinaryWithBuffer(w *bytes.Buffer) (err error) {
 	typ := uint8(tomb.typ)
-	if _, err := w.Write(types.EncodeUint8(&typ)); err != nil {
-		return 0, err
+	if _, err = w.Write(types.EncodeUint8(&typ)); err != nil {
+		return
 	}
-	size += 1
 
 	length := uint32(len(tomb.inMemTombstones))
-	if _, err := w.Write(types.EncodeUint32(&length)); err != nil {
-		return 0, err
+	if _, err = w.Write(types.EncodeUint32(&length)); err != nil {
+		return
 	}
-	size += 4
+
+	w.Grow(int(length * types.BlockidSize))
 
 	for bid, offsets := range tomb.inMemTombstones {
-
-		if _, err := w.Write(types.EncodeBlockID(&bid)); err != nil {
-			return 0, err
+		if _, err = w.Write(bid[:]); err != nil {
+			return
 		}
-		size += types.BlockidSize
 
-		cnt := uint32(len(offsets))
-		if _, err := w.Write(types.EncodeUint32(&cnt)); err != nil {
-			return 0, err
+		buf := types.EncodeSlice[int32](offsets)
+		bufLen := uint32(len(buf))
+		if _, err = w.Write(types.EncodeUint32(&bufLen)); err != nil {
+			return
 		}
-		size += 4
-
-		for _, o := range offsets {
-			if _, err := w.Write(types.EncodeInt32(&o)); err != nil {
-				return 0, err
-			}
-			size += 4
+		if _, err = w.Write(buf); err != nil {
+			return
 		}
 	}
 
 	length = uint32(len(tomb.blk2UncommitLoc))
-	if _, err := w.Write(types.EncodeUint32(&length)); err != nil {
-		return 0, err
+	if _, err = w.Write(types.EncodeUint32(&length)); err != nil {
+		return
 	}
-	size += 4
+
+	w.Grow(int(length * types.BlockidSize))
 
 	for bid, locs := range tomb.blk2UncommitLoc {
-		if _, err := w.Write(types.EncodeBlockID(&bid)); err != nil {
-			return 0, err
+		if _, err = w.Write(bid[:]); err != nil {
+			return
 		}
-		size += types.BlockidSize
 
-		cnt := uint32(len(locs))
-		if _, err := w.Write(types.EncodeUint32(&cnt)); err != nil {
-			return 0, err
+		bufLen := uint32(len(locs) * objectio.LocationLen)
+		if _, err = w.Write(types.EncodeUint32(&bufLen)); err != nil {
+			return
 		}
-		size += 4
-
 		for _, loc := range locs {
-			if _, err := w.Write(loc[:]); err != nil {
-				return 0, err
+			if _, err = w.Write(loc[:]); err != nil {
+				return
 			}
-			size += uint32(objectio.LocationLen)
 		}
-
 	}
 
 	length = uint32(len(tomb.blk2CommitLoc))
-	if _, err := w.Write(types.EncodeUint32(&length)); err != nil {
-		return 0, err
+	if _, err = w.Write(types.EncodeUint32(&length)); err != nil {
+		return
 	}
-	size += 4
+	w.Grow(int(length) * (types.BlockidSize + objectio.LocationLen + types.TxnTsSize))
 
 	for bid, loc := range tomb.blk2CommitLoc {
-		if _, err := w.Write(types.EncodeBlockID(&bid)); err != nil {
-			return 0, err
+		if _, err = w.Write(bid[:]); err != nil {
+			return
 		}
-		size += types.BlockidSize
 
-		if _, err := w.Write(loc.Loc[:]); err != nil {
-			return 0, err
+		if _, err = w.Write(loc.Loc[:]); err != nil {
+			return
 		}
-		size += uint32(objectio.LocationLen)
 
-		if _, err := w.Write(types.EncodeTxnTS(&loc.Cts)); err != nil {
-			return 0, err
+		if _, err = w.Write(types.EncodeTxnTS(&loc.Cts)); err != nil {
+			return
 		}
-		size += types.TxnTsSize
 	}
 
-	return size, nil
-
+	return
 }
 
 func (tomb *tombstoneDataWithDeltaLoc) ApplyInMemTombstones(
@@ -509,10 +489,11 @@ func (relData *blockListRelData) MarshalBinaryWithBuffer(w *bytes.Buffer) error 
 			return err
 		}
 
-		space, err := relData.tombstones.MarshalBinaryWithBuffer(w)
-		if err != nil {
+		offset := w.Len()
+		if err := relData.tombstones.MarshalBinaryWithBuffer(w); err != nil {
 			return err
 		}
+		space := uint32(w.Len() - offset)
 		//update the size of tombstones.
 		copy(w.Bytes()[pos2:pos2+4], types.EncodeUint32(&space))
 
