2e94a37435838006e6f60f5b59b7029ad0d65f8f
GreatRiver <2552853833@qq.com>
2024-08-01 10:52:57 +0800

    Remove BlockInfo

--- pkg/vm/engine/types.go
+++ pkg/vm/engine/types.go
@@ -909,7 +909,7 @@ type EntireEngine struct {
 }
 
 func IsMemtable(tblRange []byte) bool {
-	return bytes.Equal(tblRange, objectio.EmptyBlockInfoBytes)
+	return bytes.Equal(tblRange, objectio.EmptyBlockInfoInProgressBytes)
 }
 
 type EmptyRelationData struct {
